96bbcc6e12b9daf47f27b527c275211e581e1a90
lijinghui <jinghui70@gmail.com>
2026-06-27 23:36:07 +0800

    pageData增加isEmpty函数

--- src/main/java/io/github/jinghui70/rainbow/dbaccess/sql/PageData.java
+++ src/main/java/io/github/jinghui70/rainbow/dbaccess/sql/PageData.java
@@ -1,5 +1,6 @@
 package io.github.jinghui70.rainbow.dbaccess.sql;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.json.JSONUtil;
 
 import java.util.Collections;
@@ -36,22 +37,57 @@ public class PageData<T> {
         this.data = data;
     }
 
+    /**
+     * 获取查询的总记录数
+     *
+     * @return 总记录数
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * 设置查询的总记录数
+     *
+     * @param total 总记录数
+     */
     public void setTotal(int total) {
         this.total = total;
     }
 
+    /**
+     * 获取查询的结果列表
+     *
+     * @return 结果列表，如果未设置则返回空集合
+     */
     public List<T> getData() {
         return data;
     }
 
+    /**
+     * 设置查询的结果列表
+     *
+     * @param data 结果列表
+     */
     public void setData(List<T> data) {
         this.data = data;
     }
 
+    /**
+     * 判断当前分页数据是否为空
+     *
+     * @return 如果结果列表为空或 null 则返回 true，否则返回 false
+     */
+    public boolean isEmpty() {
+        return CollUtil.isEmpty(data);
+    }
+
+    /**
+     * 将当前对象转换为 JSON 格式的字符串
+     * (重写自 Object 类的 toString 方法)
+     *
+     * @return 包含 total 和 data 信息的 JSON 字符串
+     */
     @Override
     public String toString() {
         return JSONUtil.toJsonStr(this);
